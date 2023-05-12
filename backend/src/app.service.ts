446b9a148de0a91592fe4dcb8ae7a1ccd048a057
mauriziopiu <maurizio.piu427@gmail.com>
2023-05-12 16:29:17 +0200

    removed hello request

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,11 +1,10 @@
 import { Injectable } from '@nestjs/common';
-import { MongoClient } from 'mongodb';
 import { MongoService } from './mongo/mongo.service';
 
 @Injectable()
 export class AppService {
   constructor(private mongoService: MongoService) {}
   async getHello(): Promise<any> {
-    return this.mongoService.getCollection('data').find({}).toArray();
+    return 'Hello world';
   }
 }
