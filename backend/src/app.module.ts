3fdd6591b903d1db80a1a752397c926b7631e90a
Alexis Strähl <alexis.straehl@swisscom.com>
2023-05-12 18:44:14 +0200

    Call auf OSRM API

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { RoutesModule } from './routes/routes.module';
+import {HttpModule} from '@nestjs/axios'
 
 @Module({
-  imports: [],
+  imports: [RoutesModule, HttpModule],
   controllers: [AppController],
   providers: [AppService],
 })
