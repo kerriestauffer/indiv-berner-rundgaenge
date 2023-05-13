71a54e27ce0056b595675410037b00bd1352b5f1
carole <carole.leuenberger@bedag.ch>
2023-05-13 09:18:44 +0200

    icons hinzugefügt

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -10,6 +9,8 @@ import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { HttpClientModule } from '@angular/common/http';
 import { MatCardModule } from '@angular/material/card';
 import { MatExpansionModule } from '@angular/material/expansion';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatButtonModule } from '@angular/material/button';
 import { MapComponent } from './map/map.component';
 
@@ -24,6 +25,8 @@ import { MapComponent } from './map/map.component';
     HttpClientModule,
     MatCardModule,
     MatExpansionModule,
+    MatIconModule,
+    MatTooltipModule,
     MatButtonModule,
   ],
   providers: [],
